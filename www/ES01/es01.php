988af7d37c16c3cbcd99b5d4378713045d7d2a52
alessandro santoro <ale004santoro@gmail.com>
2024-12-03 12:58:24 +0000

    adsf

--- www/ES01/es01.php
+++ www/ES01/es01.php
@@ -1,4 +1,24 @@
-<?php 
-echo '<h1>Ciao ragazzi '; 
-echo 'benvenuti nel mio corso PHP!</h1>';              
-?>
+<!DOCTYPE html>
+<html lang >
+<head>
+<title>Esercitazione a</title>
+</head>
+<body>
+    <h1>Tavola pitagorica</h1>
+        <?php
+        echo '<table border="1">'; 
+        for($i=1; $i<11; $i++) 
+        {
+            echo "\n\t<tr>";
+            for($j=1; $j<11; $j++)
+            {
+                $s=$j*$i;
+                echo "<td>$s</td>";
+            }
+        }
+        echo "</table>";
+
+
+        ?>
+</body>
+</html>
